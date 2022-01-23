797e67a23fae53563ab28faf9aed4a6fc3fe311b
Jara <araj_mont@yahoo.com>
2022-01-23 18:05:13 -0500

    Fixed sign up bug
    There was an issue where when a user clicks on the sign up link, the page wouldn't open unless you right clicked and opened in a new tab. This was because of the event listeners in the javascript file from the previous iteration of the login page which aren't needed anymore since we decided to go back to keeping the login and register pages separate. As such, I have removed them.

--- Frontend/main.js
+++ Frontend/main.js
@@ -6,26 +6,6 @@ function setMessage(formElement, message) {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const container = document.querySelector("#container");
-    const registerForm = document.querySelector("#register-form");
-    const login = document.querySelector("#title-and-login");
-
-    /* If the user clicks on the sign up button */
-    document.querySelector("#register-link").addEventListener("click", e => {
-        e.preventDefault();
-        login.classList.add("hidden");
-        container.classList.add("change-width");
-        registerForm.classList.remove("hidden");
-    })
-
-    /* If the user wants to go back to the log in page */
-    document.querySelector("#login-link").addEventListener("click", e => {
-        e.preventDefault();
-        login.classList.remove("hidden");
-        container.classList.remove("change-width");
-        registerForm.classList.add("hidden");
-    })
-
     /* Submitting log in form */
     login.addEventListener("submit", e => {
         e.preventDefault();
