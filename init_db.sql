28307b241185f6c1120c395397e01bdcbda71957
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-01-25 11:11:35 -0500

    Created new database initialization script

---
+++ init_db.sql
@@ -0,0 +1,48 @@
+-- Create database
+CREATE DATABASE yellabook;
+USE yellabook;
+
+-- Create tables
+CREATE TABLE users (
+    login VARCHAR(64) NOT NULL DEFAULT '',
+    password VARCHAR(64) NOT NULL DEFAULT '',
+    PRIMARY KEY (login)
+);
+
+CREATE TABLE contacts (
+    login VARCHAR(64) NOT NULL DEFAULT '',
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    FOREIGN KEY (login) REFERENCES users (login)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (login, name)
+);
+
+CREATE TABLE emails (
+    login VARCHAR(64) NOT NULL DEFAULT '',
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    type VARCHAR(64) NOT NULL DEFAULT 'Home',
+    address VARCHAR(255) NOT NULL DEFAULT '',
+    FOREIGN KEY (login, name) REFERENCES contacts (login, name)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (login, name, address)
+);
+
+CREATE TABLE phones (
+    login VARCHAR(64) NOT NULL DEFAULT '',
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    type VARCHAR(64) NOT NULL DEFAULT 'Home',
+    number INT(10) NOT NULL DEFAULT 0,
+    FOREIGN KEY (login, name) REFERENCES contacts (login, name)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (login, name, number)
+);
+
+CREATE TABLE locations (
+    login VARCHAR(64) NOT NULL DEFAULT '',
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    type VARCHAR(64) NOT NULL DEFAULT 'Home',
+    address VARCHAR(255) NOT NULL DEFAULT '',
+    FOREIGN KEY (login, name) REFERENCES contacts (login, name)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (login, name, address)
+);
