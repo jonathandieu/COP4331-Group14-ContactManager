69a16e4a1d5e7a5632c35099265ab91107042d96
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-01-27 17:22:07 -0500

    New login.php endpoint

--- Frontend/LAMPAPI/Login.php
+++ Frontend/LAMPAPI/Login.php
@@ -2,26 +2,28 @@
 <?php
 
 	$inData = getRequestInfo();
-	
+
 	$id = 50;
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "Admin", "Admin", "ContactManager"); 	
+	$conn = new mysqli("localhost", "Admin", "Admin", "yellabook"); // Instantiate mysqli object named $conn
+
+	// If the connect_error field exists, we pass it into returnWithError() to be turned into JSON
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-		$stmt->execute();
+		$stmt = $conn->prepare("SELECT login,first_name,lastName FROM Users WHERE login=? AND password =?"); // Call the prepare() method
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]); // "ss" means that $inData is bound as a string
+		$stmt->execute(); // Executes the SQL statement
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['login'] );
 		}
 		else
 		{
@@ -31,7 +33,7 @@
 		$stmt->close();
 		$conn->close();
 	}
-	
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -42,17 +44,17 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}'; // Returns only the error with everything else empty
 		sendResultInfoAsJson( $retValue );
 	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
+
+	function returnWithInfo( $firstName, $lastName, $login )
 	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		$retValue = '{"login":' . $login . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}'; // Returns info with no error.
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
