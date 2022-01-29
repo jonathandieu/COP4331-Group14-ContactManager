3c2aeaaea48361fee8d6cd335496f2249d23e927
asachs111 <asachs111@knights.ucf.edu>
2022-01-28 21:04:47 -0500

    Freeforall08

--- Frontend/LAMPAPI/Register.php
+++ Frontend/LAMPAPI/Register.php
@@ -16,11 +16,26 @@
 		$stmt->bind_param("sss", $name, $login, $password);
 		$stmt->execute();
 		$stmt->close();
-		$conn->close();
-		echo $stmt;
-		returnWithInfo( $name, $login);
-		returnWithError("");
 
+
+
+
+		$stmt = $conn->prepare("SELECT name,login FROM users WHERE login=? AND password =?");
+		$stmt->bind_param("ss", $login, $password);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['name'], $row['login']);
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
 
 	}
 
