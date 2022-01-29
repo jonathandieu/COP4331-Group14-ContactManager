cfc1a5bb92956d384b8ae25b630028b1c35793d2
Abraham Hernandez <abeherham@knights.ucf.edu>
2022-01-28 20:18:44 -0500

    WIP: Implemented handle Register function

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -14,8 +14,8 @@ function handleLogin() {
 	document.getElementById("errorMessage").innerHTML = "";
 
 	const jsonPayload = JSON.stringify({
-		Login: login,
-		Password: password,
+		login: login,
+		password: password,
 	});
 
 	const url = urlBase + "/Login." + extension;
@@ -50,6 +50,74 @@ function handleLogin() {
 	}
 }
 
+function handleRegister() {
+	let fullName = "";
+	let username = "";
+
+	const firstName = document.getElementById("firstName").value.trim();
+	const lastName = document.getElementById("lastName").value.trim();
+	const login = document.getElementById("username").value.trim();
+	const password = document.getElementById("password").value.trim();
+	const confirmPassword = document
+		.getElementById("confirmPassword")
+		.value.trim();
+
+	if (
+		firstName === "" ||
+		lastName === "" ||
+		username === "" ||
+		password === "" ||
+		confirmPassword === ""
+	) {
+		document.getElementById("errorMessage").innerHTML =
+			"One or more fields are empty";
+		return;
+	}
+
+	if (password !== confirmPassword) {
+		document.getElementById("errorMessage").innerHTML =
+			"Passwords do not match";
+		return;
+	}
+
+	const jsonPayload = JSON.stringify({
+		name: firstName + " " + lastName,
+		login: login,
+		password: password,
+	});
+
+	const url = urlBase + "/Register." + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try {
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				let jsonObject = JSON.parse(xhr.responseText);
+				error = jsonObject.error;
+
+				if (error != "") {
+					document.getElementById("errorMessage").innerHTML =
+						"An error has occured. Try again.";
+					return;
+				}
+
+				fullName = jsonObject.name;
+				username = jsonObject.login;
+
+				saveCookie();
+
+				window.location.href = "dashboard.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	} catch (err) {
+		document.getElementById("errorMessage").innerHTML = err.message;
+	}
+}
+
 function saveCookie() {
 	let minutes = 60;
 	let date = new Date();
@@ -57,7 +125,7 @@ function saveCookie() {
 	document.cookie =
 		"name=" +
 		fullName +
-		",username=" +
+		",login=" +
 		username +
 		";expires=" +
 		date.toGMTString();
