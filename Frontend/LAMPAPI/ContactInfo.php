b5ebe18fc12b8e67b09bfbbea8e0c000b9b0d852
asachs111 <asachs111@knights.ucf.edu>
2022-02-03 23:00:53 -0500

    contactinfo00

--- Frontend/LAMPAPI/ContactInfo.php
+++ Frontend/LAMPAPI/ContactInfo.php
@@ -1,39 +1,68 @@
+
 <?php
-	$inData = getRequestInfo();
 
-	$color = $inData["color"];
-	$userId = $inData["userId"];
+$inData = getRequestInfo();
+$login = $inData["login"];
+$cname = $inData["cname"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+$conn = new mysqli("localhost", "Admin", "Admin", "yellabook");
+if( $conn->connect_error )
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE login=? AND cname =?");
+    $stmt->bind_param("ss", $inData["login"], $inData["cname"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    returnWithInfo( $row['name'], $row['login']);
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+    $stmt = $conn->prepare("SELECT * FROM emails WHERE login=? AND cname =?");
+    $stmt->bind_param("ss", $inData["login"], $inData["cname"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    returnWithInfo( $row['type'], $row['address']);
+
+    $stmt = $conn->prepare("SELECT * FROM phones WHERE login=? AND cname =?");
+    $stmt->bind_param("ss", $inData["login"], $inData["cname"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    returnWithInfo( $row['type'], $row['number']);
+
+    $stmt = $conn->prepare("SELECT * FROM locations WHERE login=? AND cname =?");
+    $stmt->bind_param("ss", $inData["login"], $inData["cname"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    returnWithInfo( $row['type'], $row['address']);
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"name":"","login":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function returnWithInfo( $name, $login )
+{
+    $retValue = '{"name":"' . $name . '","login":"' . $login . '","error":""}';
+    sendResultInfoAsJson( $retValue );
+}
 
 ?>
