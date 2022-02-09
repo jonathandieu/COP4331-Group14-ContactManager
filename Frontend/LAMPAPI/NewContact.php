22e6b1e85b6caaf1b2fe1352196734c21a182d22
asachs111 <asachs111@knights.ucf.edu>
2022-02-09 16:21:30 -0500

    ltype02

--- Frontend/LAMPAPI/NewContact.php
+++ Frontend/LAMPAPI/NewContact.php
@@ -52,8 +52,8 @@
         // Locations
         if (strlen($laddress) > 0)
         {
-			//if (strcmp($ltype,"") == 0)
-			$ltype = "Home";
+			if (strlen($ltype) == 0)
+				$ltype = "Home";
             $stmt = $conn->prepare("INSERT into locations (login,cname,type,address) VALUES(?,?,?,?)");
             $stmt->bind_param("ssss", $login, $cname, $ltype, $laddress);
 	    	$stmt->execute();
